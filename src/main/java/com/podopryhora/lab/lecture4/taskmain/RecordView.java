622326211a5b0ca977b74b3ba773561edd0bdec4
Nick Underhill <m.podopryhora@gmail.com>
2020-11-08 21:45:48 +0200

    Task 5. Add properties files with localization

--- src/main/java/com/podopryhora/lab/lecture4/taskmain/RecordView.java
+++ src/main/java/com/podopryhora/lab/lecture4/taskmain/RecordView.java
@@ -1,8 +1,23 @@
 package com.podopryhora.lab.lecture4.taskmain;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class RecordView {
 
-    public void printMessage(String message) {
+    // Resource Bundle Installation's
+    static String MESSAGES_BUNDLE_NAME = "messages";
+    public static final ResourceBundle bundle =
+            ResourceBundle.getBundle(
+                    MESSAGES_BUNDLE_NAME, new Locale("en"));
+//                    MESSAGES_BUNDLE_NAME, new Locale("ua", "UA"));
+
+
+    public void printText(String message) {
         System.out.println(message);
+    }
+
+    public void printLocalizedMessage(String message) {
+        printText(bundle.getString(message));
     }
 }
---
+++ src/main/java/com/podopryhora/lab/lecture4/taskmain/TextConstants.java
@@ -0,0 +1,11 @@
+package com.podopryhora.lab.lecture4.taskmain;
+
+public class TextConstants {
+
+    public static final String MSG_WRONG_EMAIL_FORMAT = "input.error.email";
+    public static final String MSG_WRONG_NAME_FORMAT = "input.error.name";
+    public static final String MSG_SUCCESS = "input.info.success";
+    public static final String MSG_ENTER_NAME = "input.prompt.name";
+    public static final String MSG_ENTER_LAST_NAME = "input.prompt.lastname";
+    public static final String MSG_ENTER_EMAIL = "input.prompt.email";
+}
